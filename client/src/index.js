ed9bf989f7be639973df7f9f022eab5b0dbe22eb
MuttakinHasib <hasibmolla28@gmail.com>
2021-03-22 19:46:39 +0600

    bug :bug:

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 
-import './bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HomeScreen = () => {
-  return <div></div>;
+  return <div className="bg-[#ff3366] h-screen"></div>;
 };
 
 export default HomeScreen;
